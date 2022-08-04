aa5963a909947b1c7ed194ecf811037407398b4d
AlinaRovinska <aa.rovinska@gmail.com>
2022-08-04 14:34:43 +0300

    fix style

--- src/app.js
+++ src/app.js
@@ -86,4 +86,4 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 let celciusLink = document.querySelector("#celcius-link");
 celciusLink.addEventListener("click", displayCelciusTemperature);
 
-search("New York");
+search("Kyiv");
